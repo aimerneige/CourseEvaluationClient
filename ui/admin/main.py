810090be148971d2b3f8166ae417c10848348146
Aimer Neige <aimer.neige.soft@gmail.com>
2022-01-05 23:02:10 +0800

    update

---
+++ ui/admin/main.py
@@ -0,0 +1,36 @@
+#!/usr/env/bin python3
+# -*- coding: utf-8 -*-
+# Author: Aimer Neige
+# Mail: aimer.neige@aimerneige.com
+# LICENSE: AGPLv3 (https://www.gnu.org/licenses/agpl-3.0.txt)
+
+
+from PyQt5.QtWidgets import QMainWindow
+
+
+window_title = "Admin Manage"
+window_width = 300
+window_height = 140
+
+
+class AdminMainWindow(QMainWindow):
+    def __init__(self, father) -> None:
+        super().__init__()
+        self.father = father
+        self.initWindow()
+        self.initUI()
+        self.center()
+
+    def initWindow(self) -> None:
+        """
+        Init window properties.
+        """
+        self.setWindowTitle(window_title)
+        self.setFixedWidth(window_width)
+        self.setFixedHeight(window_height)
+
+    def initUI(self) -> None:
+        """
+        Init UI widgets and layout.
+        """
+        pass
